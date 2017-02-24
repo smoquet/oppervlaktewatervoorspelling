e57d194ce0d67392303fe6ca5a37c2a4f642fc8b
smoquet <sr_moquette@yahoo.com>
2017-02-24 14:13:19 +0100

    implements mannings formula in exchange of water between edges, also some PEP8

--- class_structure.py
+++ class_structure.py
@@ -1,6 +1,9 @@
 import helpers
-
+   
+    
 def get_BFS_iteration_sequence_of_nodes(start_node):
+    '''returns a list of nodes in a breadth first search order
+    '''
     if type(start_node) != EndNode:
         raise ValueError('First node to iterate from is not of type EndNode')
     q = []
@@ -24,131 +27,200 @@ def get_BFS_iteration_sequence_of_nodes(start_node):
     return order_of_bfs_iteration+endnodes_unknown_flow
 
 class Edge(object):
+    '''represents an object in the stroomgebied
     '''
-    represents an object in the stroomgebied
-    expects a name, and height of water as flaot
-    '''
+    
+    
     def __init__(self, name, water_volume, stub_param):
+        # params set on init
         self.name = str(name)
         self.water_volume = water_volume 
         self.stub_param =  float(stub_param)
         self.nodes = []
-        self.height_volume_ratio = 1.0
-        self.water_level = self.height_volume_ratio*self.water_volume
-        self.water_volume_passage = 0.0
         
+        # params to be set on init eventually
         self.manning_coefficient = 0.013
-        self.slope = 0.0005
-        self.length = 10.0 
-        self.talus = 0.5 #talud 1 meter omlaag is 0.5 meter opzij
+        self.length = 100.0 
+        self.talus = 0.0 #talud 1 meter omlaag is 0.5 meter opzij
         self.bottom_width = 5.0 #bodem breedte
         self.bottom_level = 1.0 #bodem niveau
-        self.water_depth = self.water_level-self.bottom_level
+        
+        # params to calculate on initialisation
+        self.water_depth_to_volume_ratio = 1/(self.bottom_width*self.length)
+
+        # params to calculate after water displacement
+        self.water_depth = self.water_depth_to_volume_ratio*self.water_volume
+        self.water_level = self.water_depth+self.bottom_level
         self.water_surface_width = self.bottom_width+2.0*(self.water_depth*self.talus)
         self.wetted_perimeter = self.bottom_width + 2.0*(((self.water_surface_width-self.bottom_width)/2.0)**2.0 + self.water_depth**2.0)**0.5
         self.cross_sectional_area_of_flow = self.water_depth*(self.bottom_width+self.water_surface_width)/2.0
         self.hydraulic_radius = self.cross_sectional_area_of_flow/self.wetted_perimeter
+        # this one is calculated seperatly after water displacement
+        self.volume_of_water_passage = 0.0
+        
+        # params to calculate before water displacement
+        self.slope = 0.0
         self.velocity = 1/self.manning_coefficient*self.hydraulic_radius**(2.0/3.0)*self.slope**(1.0/2.0)
         self.discharge = self.velocity*self.cross_sectional_area_of_flow
         
     def __str__(self):
-        return 'Edge ' +self.name +' ' + str([node.__str__() for node in self.nodes])
+        return 'Edge ' +self.name +' level: ' +str(self.water_level)+ ' volume: '+str(self.water_volume)+' Q: '+str(self.volume_of_water_passage) 
+    
     def get_name(self):
         return self.name
+    
     def get_water_level(self):
         return self.water_level
-    def adjust_height_to_water_volume(self):
-        self.water_level = self.height_volume_ratio*self.water_volume
+    
+    def get_slope(self):
+        return self.slope
+    
+    def adjust_parameters_after_water_displacement(self):
+        self.water_depth = self.water_depth_to_volume_ratio*self.water_volume
+        self.water_level = self.water_depth+self.bottom_level
+        self.water_depth+self.bottom_level
+        self.water_surface_width = self.bottom_width+2.0*(self.water_depth*self.talus)
+        self.wetted_perimeter = self.bottom_width + 2.0*(((self.water_surface_width-self.bottom_width)/2.0)**2.0 + self.water_depth**2.0)**0.5
+        self.cross_sectional_area_of_flow = self.water_depth*(self.bottom_width+self.water_surface_width)/2.0
+        self.hydraulic_radius = self.cross_sectional_area_of_flow/self.wetted_perimeter
+        
+    
+    def calculate_set_and_return_slope_and_calculate_discharge(self, water_level_at_end_of_edge):
+        water_level_difference_between_middle_and_end = self.water_level-water_level_at_end_of_edge
+        self.slope = abs(water_level_difference_between_middle_and_end/(self.length/2)) #divided by two, because self.water_level is the level in the middle of the waterway
+        try:
+            self.velocity = 1/self.manning_coefficient*self.hydraulic_radius**(2.0/3.0)*self.slope**(1.0/2.0)
+        except ValueError:
+            raise ValueError('Hydraulic Radius is negative, this is impossible')
+        self.discharge = self.velocity*self.cross_sectional_area_of_flow
+        return self.slope
+    
     def get_water_volume(self):
         return self.water_volume
+    
     def get_height_volume_ratio(self):
         return self.height_volume_ratio
+    
+    def get_water_depth_to_volume_ratio(self):
+        return self.water_depth_to_volume_ratio
+    
+    def get_discharge_Q(self):
+        return self.discharge
+    
     def adjust_water_volume(self, volume):
         self.water_volume += volume
-        self.adjust_height_to_water_volume()
+        self.adjust_parameters_after_water_displacement()
         if volume <0:
-            self.water_volume_passage=volume
+            self.volume_of_water_passage=volume
+    
     def add_node(self,*args):
         for a in args:
             self.nodes.append(a)
+    
     def get_other_node(self, node):
         for n in self.nodes:
             if n != node:
                 return n
+    
     def get_stub_param(self):
         return self.stub_param
-    def get_water_volume_passage(self):
-        return self.water_volume_passage
+    
+    def get_volume_of_water_passage(self):
+        return self.volume_of_water_passage
 
 
 
 class Node(object):
+
+    
     def __init__(self, *args):
         self.edges = (args)
+    
     def __str__(self):
         return "Node " + str([(e.name, e.water_level, e.stub_param) for e in self.edges])
+    
     def get_edges(self):
         return self.edges
+    
     def get_connected_nodes(self):
         all_nodes = [edge.get_other_node(self) for edge in self.edges]
         return all_nodes
+    
     def displace_water(self):
-        '''displaces a Q between all connected nodes so according to the waterheight and neigbouring params'''
-        helpers.displacement_stub(self.edges)
+        '''displaces a Q between all connected nodes according to mannings formula'''
+        helpers.displace_water_between_edges(self.edges)
+
 
 class EndNode(Node):
+
+    
     def __init__(self, edge, discharge):
         self.edges = [edge]
         self.discharge = float(discharge)
+        
     def add_or_reduce_water(self):
         self.edges[0].adjust_water_volume(self.discharge)
 
+
 class EndNodeWithUnknownFlow(Node):
-    def __init__(self, edge, threshold):
+    
+    
+    def __init__(self, edge, threshold, water_level, qh_relationship):
         self.edges = [edge]
-        self.threshold = threshold
+        self.threshold = threshold # absolute value, not relative to bottom of waterway
+        self.water_level = water_level
+        self.qh_relationship = qh_relationship
+        
     def get_threshold(self):
         return self.threshold
+    
     def displace_water(self):
         helpers.edge_flow_stub(self)
     
         
-        
 class Graph(object):
+    
+    
     def __init__(self, name, edges, endnodes, nodes):
         self.name = name
         self.endnodes  = endnodes
         self.nodes = nodes
         self.edges = edges
         self.start_node = None
+    
     def __str__(self):
         return 'Graph with '+str(len(self.nodes))+' nodes and '+str(len(self.edges))+ ' edges'
+    
     def get_edges(self):
         return self.edges
+    
     def add_node(self, node):
         self.nodes.append(node)
+    
     def add_edge(self, edge):
         self.edges.append(edge)
+    
     def set_node_sequence(self, start_node):
         self.nodes = get_BFS_iteration_sequence_of_nodes(start_node)
+    
     def perform_exterior_flow(self):
         for en in self.endnodes:
             en.add_or_reduce_water()
-    def displace_water_between_nodes(self):
+    
+    def displace_water_between_edges(self):
         for n in self.nodes:
             n.displace_water()
         
    
 def test_polder_simple():
-      
     # test displacement between four edges and three end points
     e1 = Edge(name = '1', water_volume= 2, stub_param = 0.05)
     e2 = Edge(name = '2', water_volume= 2, stub_param = 0.05)
     e3 = Edge(name = '3', water_volume= 2, stub_param = 0.05)
     e4 = Edge(name = '4', water_volume= 2, stub_param = 0.05)
     end_node1 = EndNode(edge=e1,discharge=2)
-    end_node_UF3 = EndNodeWithUnknownFlow(edge=e3,threshold=10)
-    end_node_UF4 = EndNodeWithUnknownFlow(edge=e4,threshold=10)
+    end_node_UF3 = EndNodeWithUnknownFlow(edge=e3,threshold=4, water_level=0, qh_relationship=helpers.qh_relationship_end_node_unknown_flow_1)
+    end_node_UF4 = EndNodeWithUnknownFlow(edge=e4,threshold=4, water_level=0, qh_relationship=helpers.qh_relationship_end_node_unknown_flow_1)
     n12 = Node(e1,e2,e4)
     n23 = Node(e2,e3)
     e1.add_node(end_node1)
@@ -166,12 +238,42 @@ def test_polder_simple():
     
     polder = Graph(name = 'polder', edges = edges, endnodes = endnodes, nodes = nodes)
     polder.set_node_sequence(end_node1)
-    for i in range(800):
+    for i in range(10):
         polder.perform_exterior_flow()
-#         print [[edge.get_name(),edge.get_water_volume(), edge.get_water_volume_passage()] for edge in polder.get_edges()]
-        polder.displace_water_between_nodes()
-        print [[edge.get_name(),edge.get_water_volume(), edge.get_water_volume_passage()] for edge in polder.get_edges()]
+#         print [[edge.get_name(),edge.get_water_volume(), edge.get_volume_of_water_passage()] for edge in polder.get_edges()]
+        polder.displace_water_between_edges()
+        print 'iteration nr: ',i,"    ",[[edge.get_name(),edge.get_water_volume(), edge.get_volume_of_water_passage()] for edge in polder.get_edges()]
 
-test_polder_simple()
+# test_polder_simple()
         
         
+        
+def test_polder_2gemaal_3sloot():
+    e1 = Edge(name = '1', water_volume= 600.0, stub_param = 0.05)
+    e2 = Edge(name = '2', water_volume= 600.0, stub_param = 0.05)
+    e3 = Edge(name = '3', water_volume= 600.0, stub_param = 0.05)
+    end_node1 = EndNode(edge=e1,discharge=2.0)
+    end_node3 = EndNode(edge=e3,discharge=-2.0)    
+    n12 = Node(e1,e2)
+    n23 = Node(e2,e3)
+    e1.add_node(end_node1)
+    e1.add_node(n12)
+    e2.add_node(n12)
+    e2.add_node(n23)
+    e3.add_node(n23)
+    e3.add_node(end_node3)
+    
+    edges = [e1,e2,e3]
+    endnodes = [end_node1,end_node3] 
+    nodes = [n12,n23]
+    
+    polder = Graph(name = 'polder', edges = edges, endnodes = endnodes, nodes = nodes)
+    polder.set_node_sequence(end_node1)
+    for i in range(100):
+        polder.perform_exterior_flow()
+#         print [[edge.get_name(),edge.get_water_volume(), edge.get_volume_of_water_passage()] for edge in polder.get_edges()]
+        polder.displace_water_between_edges()
+        print 'iteration nr: ',i,"    ",[[edge.get_name(),'Vol',edge.get_water_volume(), 'Slope', edge.get_slope(), 'Q_real', edge.get_volume_of_water_passage()] for edge in polder.get_edges()]
+
+test_polder_2gemaal_3sloot()
+    
--- helpers.py
+++ helpers.py
@@ -1,40 +1,95 @@
+def edge_water_level_is_lower(edge, water_level):
+    return edge.get_water_level() < water_level
 
-def displacement_stub(edges):
+def displace_water_between_edges(edges):
+    '''expects: edges that are connected with each other
+    1: get average_water_level from edges
+    2: set slopes of edges according to this average height, their heights and lengths
+    3: get Q from edges that are net givers of water, and subtract this amount from their water_volume
+    4: give the Q to the net receiving edges on the ratio of their respective slopes (the most sloped ones get more)
+    returns nothing 
     '''
-    displaces water between any number of edges as follows:
-    difference between the average height and the height of each edge * the average param_stub
-    '''
-    
-    average_volume = 0
-    average_stub_param = 0
-    edge_count  = 0 
+    average_water_level = 0.0
+#     edges_dict = {}
+    water_receiving_edges = []
+    total_Q_to_give_to_receiving_edges = 0.0
+    added_lower_water_levels = 0.0
     for edge in edges:
-        edge_count += 1
-        average_volume += edge.get_water_volume()
-        average_stub_param += edge.get_stub_param()
-    average_volume /= edge_count 
-    average_stub_param /= edge_count
-    
+        edge_water_level = edge.get_water_level()
+        average_water_level += edge_water_level
+#         edges_dict[edge] = [edge_water_level]
+    average_water_level /= len(edges)
     for edge in edges:
-        volume = edge.get_water_volume()
-        displacement_quantity = average_volume-volume
-        if abs(displacement_quantity)<0.00005:
-            pass
-        else:
-            edge.adjust_water_volume(displacement_quantity*average_stub_param)
+        slope = edge.calculate_set_and_return_slope_and_calculate_discharge(average_water_level)
+#         edges_dict[edge].append(slope) 
+        if slope == 0.0:
+            water_receiving_edges.append([edge, 0]) 
+        elif not edge_water_level_is_lower(edge, average_water_level): # giver of water
+            water_displacement_quantity = edge.get_discharge_Q()
+            total_Q_to_give_to_receiving_edges +=water_displacement_quantity 
+            edge.adjust_water_volume(-1*abs(water_displacement_quantity))
+        elif edge_water_level_is_lower(edge, average_water_level): # receiver of wter
+            edge_water_level = edge.get_water_level()
+            difference_in_water_level = average_water_level-edge_water_level
+            added_lower_water_levels += difference_in_water_level
+            water_receiving_edges.append([edge, difference_in_water_level])
+    for receivers in water_receiving_edges:
+        edge = receivers[0]
+        difference_in_water_level = receivers[1]
+        ratio_of_total_Q_to_be_given_to_this_edge = difference_in_water_level/added_lower_water_levels
+        Q_to_give_to__this_receiving_edge = total_Q_to_give_to_receiving_edges*ratio_of_total_Q_to_be_given_to_this_edge
+        edge.adjust_water_volume(Q_to_give_to__this_receiving_edge)
+        
+
+# def edge_flow_stub(end_node_unknown_flow):
+#     displacement_quantity = 0
+#     height_difference = 0
+#     edge = end_node_unknown_flow.get_edges()[0]
+#     edge_height=edge.get_water_level()
+#     edge_volume_height_ratio = edge.get_height_volume_ratio()
+#     threshold = end_node_unknown_flow.get_threshold()
+#  
+#     if edge_height<threshold:
+#         pass
+#     else:
+#         height_difference = edge_height-threshold
+#         displacement_quantity = height_difference/edge_volume_height_ratio*-1
+#         edge.adjust_water_volume(displacement_quantity)
 
 def edge_flow_stub(end_node_unknown_flow):
-    displacement_quantity = 0
-    height_difference = 0
-    edge = end_node_unknown_flow.get_edges()[0]
-    edge_height=edge.get_water_level()
-    edge_volume_height_ratio = edge.get_height_volume_ratio()
-    threshold = end_node_unknown_flow.get_threshold()
-
-    if edge_height<threshold:
-        pass
+    node_has_higher_level_than_threshold = end_node_unknown_flow.water_level < end_node_unknown_flow.threshold
+    edge_water_level = end_node_unknown_flow.edges[0].get_water_level()
+    edge_has_higher_level_than_threshold = edge_water_level > end_node_unknown_flow.threshold
+     
+    if not node_has_higher_level_than_threshold and not edge_has_higher_level_than_threshold:
+        pass # both levels are to low for movement
+    elif node_has_higher_level_than_threshold and not edge_has_higher_level_than_threshold:
+        # node gives water to edge
+        water_level_difference = end_node_unknown_flow.water_level-end_node_unknown_flow.threshold
+        displacement_quantity = end_node_unknown_flow.qh_relationship(h=water_level_difference)
+        end_node_unknown_flow.edges[0].adjust_water_volume(-1*displacement_quantity)
+    elif not node_has_higher_level_than_threshold and edge_has_higher_level_than_threshold:
+        # edge gives water to node
+        water_level_difference = edge_water_level-end_node_unknown_flow.threshold
+        displacement_quantity = end_node_unknown_flow.qh_relationship(h=water_level_difference)
+        end_node_unknown_flow.edges[0].adjust_water_volume(displacement_quantity)
     else:
-        height_difference = edge_height-threshold
-        displacement_quantity = height_difference/edge_volume_height_ratio*-1
-        edge.adjust_water_volume(displacement_quantity)
-        
+        #both are higher than threshold, the difference between two hights dictates the direction and q
+        water_level_difference = end_node_unknown_flow.water_level-edge_water_level
+        displacement_quantity = end_node_unknown_flow.qh_relationship(water_level_difference)
+        end_node_unknown_flow.edges[0].adjust_water_volume(displacement_quantity)
+
+def qh_relationship_end_node_unknown_flow_1(Q = None, h=None):
+    if not Q and not h:
+        raise StandardError('No Q or h passed on to Qh-function')
+    elif Q and h:
+        raise StandardError('Both Q and h passed on to Qh-function')
+    elif Q and not h:
+        h = Q*0.5 # voor elke Q krijg je 0.5 h
+        return h
+    else:
+        Q = h/0.5 # voor elke h krijg je 2 Q
+        return Q
+
+    
+    
